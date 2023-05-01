14394d5295ca797e7bd4bd96e1648715c58d4b0a
Sharon N Nabiryo <sharongless37@gmail.com>
2023-05-01 00:05:12 -0400

    all

--- src/components/Pokedata.js
+++ src/components/Pokedata.js
@@ -1,6 +1,4 @@
-import { type } from "@testing-library/user-event/dist/type";
-
-export default [
+const Pokedata = [
   { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
   { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
   { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
@@ -10,3 +8,5 @@ export default [
   { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
   { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
 ];
+
+export default Pokedata;
